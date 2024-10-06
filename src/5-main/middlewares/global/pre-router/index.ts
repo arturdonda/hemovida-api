1503e67186b228e265528459c0b61cb147206990
Artur Donda <artur.donda@outlook.com>
2024-10-06 16:30:55 -0300

    Add helmet

--- src/5-main/middlewares/global/pre-router/index.ts
+++ src/5-main/middlewares/global/pre-router/index.ts
@@ -1,7 +1,14 @@
 import { PreRouterMiddlewareProtocol } from '@main/protocols';
 import { configureCors } from './cors';
+import { configureHelmet } from './helmet';
 import { injectTracer } from './inject-tracer';
 import { configureJsonBody } from './json-body-parser';
 import { configureUrlEncodedBody } from './urlencoded-body-parser';
 
-export const GLOBAL_PRE_ROUTER_MIDDLEWARES: PreRouterMiddlewareProtocol[] = [configureCors, injectTracer, configureJsonBody, configureUrlEncodedBody];
+export const GLOBAL_PRE_ROUTER_MIDDLEWARES: PreRouterMiddlewareProtocol[] = [
+	configureCors,
+	configureHelmet,
+	injectTracer,
+	configureJsonBody,
+	configureUrlEncodedBody,
+];
