0bf1c063234f7dbe5c6d57ca098c50bf9d6d88c9
Artur Donda <artur.donda@outlook.com>
2024-10-09 19:36:24 -0300

    Add delete user usecase

---
+++ src/2-application/use-cases/user/delete.ts
@@ -0,0 +1,22 @@
+import { Tracer } from '@domain/app';
+import { DeleteUserUsecaseProtocol } from '@application/protocols/use-cases/user';
+import { DatabaseProtocol } from '@application/protocols/infra';
+import { NotFoundError } from '@application/errors';
+
+export class DeleteUserUsecase extends DeleteUserUsecaseProtocol {
+	constructor(tracer: Tracer, private readonly userRepository: DatabaseProtocol.Repositories.Public.User) {
+		super(tracer);
+	}
+
+	protected validateParams(params: DeleteUserUsecaseProtocol.Params): void {
+		return;
+	}
+
+	protected async main({ id }: DeleteUserUsecaseProtocol.Params): Promise<DeleteUserUsecaseProtocol.Result> {
+		const user = await this.userRepository.getOne({ id });
+
+		if (user === null) throw new NotFoundError('User');
+
+		return this.userRepository.delete(user);
+	}
+}
