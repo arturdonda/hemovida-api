8d1a2e143078334d8583fe3d1787ce23168eb7a0
Artur Donda <artur.donda@outlook.com>
2024-10-19 17:38:07 -0300

    Add get all sessions usecase

--- src/2-application/protocols/use-cases/session/get-one.ts
+++
@@ -1,9 +0,0 @@
-import { Usecase } from '@domain/app';
-import { Session } from '@domain/entities';
-
-export abstract class GetOneSessionUsecaseProtocol extends Usecase<GetOneSessionUsecaseProtocol.Params, GetOneSessionUsecaseProtocol.Result> {}
-
-export namespace GetOneSessionUsecaseProtocol {
-	export type Params = Partial<Pick<Session, Session.UniqueFields>>;
-	export type Result = Session;
-}
---
+++ src/2-application/use-cases/session/get-all.ts
@@ -0,0 +1,23 @@
+import { Tracer } from '@domain/app';
+import { Session } from '@domain/entities';
+import { DatabaseProtocol } from '@application/protocols/infra';
+import { GetAllSessionsUsecaseProtocol } from '@application/protocols/use-cases/session';
+import { PageParams } from '@application/helpers';
+
+export class GetAllSessionsUsecase extends GetAllSessionsUsecaseProtocol {
+	constructor(tracer: Tracer, private readonly sessionRepository: DatabaseProtocol.Repositories.Public.Session) {
+		super(tracer);
+	}
+
+	protected validateParams(params: GetAllSessionsUsecaseProtocol.Params): void {
+		return;
+	}
+
+	protected async main(params: GetAllSessionsUsecaseProtocol.Params): Promise<GetAllSessionsUsecaseProtocol.Result> {
+		const { pageNumber, pageSize, sortBy, sortDirection, ...searchableFields } = params;
+
+		const pageParams = new PageParams<Session>({ pageNumber, pageSize, sortBy, sortDirection });
+
+		return this.sessionRepository.getAll({ pageParams, ...searchableFields });
+	}
+}
