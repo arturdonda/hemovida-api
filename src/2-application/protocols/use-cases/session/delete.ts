81098757f162a7616186a1220c50fd61bc754745
Artur Donda <artur.donda@outlook.com>
2024-10-23 07:19:10 -0300

    Remove delete session usecase protocol

--- src/2-application/protocols/use-cases/session/delete.ts
+++
@@ -1,9 +0,0 @@
-import { Usecase } from '@domain/app';
-import { Session } from '@domain/entities';
-
-export abstract class DeleteSessionUsecaseProtocol extends Usecase<DeleteSessionUsecaseProtocol.Params, DeleteSessionUsecaseProtocol.Result> {}
-
-export namespace DeleteSessionUsecaseProtocol {
-	export type Params = Pick<Session, 'id'>;
-	export type Result = void;
-}
