6b12633be2ce7f166e3a2a4949e978c0036f6f56
Artur Donda <artur.donda@outlook.com>
2024-10-24 07:14:38 -0300

    Rename services

--- src/2-application/protocols/infra/index.ts
+++ src/2-application/protocols/infra/index.ts
@@ -1,6 +1,2 @@
 export * from './database';
-export * from './email-service';
-export * from './hash-service';
-export * from './ip-lookup-service';
-export * from './token-service';
-export * from './user-agent-lookup-service';
+export * from './services';
---
+++ src/3-infra/services/index.ts
@@ -0,0 +1,5 @@
+export * from './email';
+export * from './hash';
+export * from './ip-lookup';
+export * from './token';
+export * from './user-agent-lookup';
